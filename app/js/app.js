042c931caea3f0aa1926d174ef6e3054c19ce867
Bill Heaton <pixelhandler@gmail.com>
2013-02-28 09:53:50 +0000

    finished first draft of build w/ rake-pipeline-web-filters

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,16 @@
-/*global App*/
+var require = minispade.require;
 
+require("jquery-1.8.3.min");
+require("handlebars-1.0.0.rc.2");
+require("ember");
+require("ember-data");
+
+/*global App*/
 window.App = Ember.Application.create();
 
+require("routes");
+require("controllers");
+require("helpers");
+require("models");
+require("views");
+require("fixtures");
